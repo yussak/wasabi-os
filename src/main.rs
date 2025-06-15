b2fb399a52c952583d0327047af2fa318d9144cd
yussak <yusuke.sakuraba0213@gmail.com>
2025-06-15 10:04:59 +0900

    main.rsのno_std化

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// stdライブラリ（クレート）を使わない宣言
+#![no_std]
+// main関数を使わない宣言
+#![no_main]
+
+// 名前修飾（マングリング）の無効化
+#[no_mangle]
+fn efi_main() {
+    // println!("Hello, world!");
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
