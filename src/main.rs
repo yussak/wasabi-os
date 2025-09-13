b5a4a61a527fe9119f599df1ea9bde57ffb8a2f1
yussak <yusuke.sakuraba0213@gmail.com>
2025-09-13 21:55:18 +0900

    不要コード削除

--- src/main.rs
+++ src/main.rs
@@ -113,15 +113,6 @@ pub fn hlt() {
 // 名前修飾（マングリング）の無効化
 #[no_mangle]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    // let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
-    // let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    // let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
-    // let vram = unsafe {
-    //     slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
-    // };
-    // for e in vram {
-    //     *e = 0xffffff;
-    // }
     let mut vram = init_vram(efi_system_table).expect("init_vram failed");
     for y in 0..vram.height {
         for x in 0..vram.width {
